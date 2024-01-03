c4e48a1f36df172c5d1279f594b5c9c6f6c27241
davidajimati <ajimatidavid@gmail.com>
2024-01-03 16:15:02 +0100

    0-uniq_users.sql tested

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,10 @@
 -- script that creates a table users
 -- check if the table exists before attempting to create it
-IF NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+
 -- define the table and it's columns
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
-END IF;
