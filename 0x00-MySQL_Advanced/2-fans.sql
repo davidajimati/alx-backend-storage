a946c0a66fd59904c08170e69143a45e6b8a18c8
davidajimati <ajimatidavid@gmail.com>
2024-01-04 21:24:06 +0100

    2-fans.sql modified and tested

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- Script to select only some fields from a table
 
-SELECT origin, fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-ORDER BY fans DESC;
+GROUP BY origin
+ORDER BY nb_fans DESC;
