d7eb1a30ecaa0052ee7091949ad02854a81a427e
davidajimati <ajimatidavid@gmail.com>
2024-01-05 17:21:17 +0100

    10-div.sql created

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- SQL script that creates a function SafeDiv that divides (and returns) the first by
+-- the second number or returns 0 if the second number is equal to 0.
+
+DELIMITER //
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+BEGIN
+    DECLARE answer INT;
+    IF b <> 0 THEN
+        SET answer =  a / b;
+    ELSE
+        SET answer = 0;
+    END IF
+        RETURN answer;
+END;
+//
+
+DELIMITER ;
+
