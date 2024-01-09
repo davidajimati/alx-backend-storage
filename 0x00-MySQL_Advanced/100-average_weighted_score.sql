47f3b18f3fd11d07e1b3f7cd93dd736a3a08c35d
davidajimati <ajimatidavid@gmail.com>
2024-01-09 10:12:46 +0100

    0x00-MySQL_Advanced updated

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,12 @@
+--  script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
+DELIMITER //
+
+DROP PROCEDURE IF EXSISTS ComputeAverageWeightedScoreForUser;
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN u_id INT)
+BEGIN
+
+END
+//
+
+DELIMITER ;
